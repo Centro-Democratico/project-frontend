f624067580b3012c169e84a7d6eef85548aebe70
David Santiago Vargas Parra <dvargaspa@unal.edu.co>
2026-07-06 19:17:03 -0500

    Simplify Vite config by removing Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(() => {
-  return {
-    plugins: [vue(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://127.0.0.1:8000', //Cambiar por la URL de producción de Django cuando despliegue
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''), 
-        },
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
-    },
-  };
-});
+    }
+  }
+})
