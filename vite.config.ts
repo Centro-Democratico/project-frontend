c5a72f21e7ed6bb4ceb05cc1a9441674854a51d0
David Santiago Vargas Parra <dvargaspa@unal.edu.co>
2026-07-06 19:27:51 -0500

    correccion

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(() => {
+  return {
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8000', //Cambiar por la URL de producción de Django cuando despliegue
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''), 
+        },
       }
-    }
-  }
-})
+    },
+  };
+});
