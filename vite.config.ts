bf3c7ea8f397e1c85c1686b748cfdc99bdd6c68b
dangomezma <dangomezma@unal.edu.co>
2026-07-07 22:51:17 -0500

    frontend up-to-date

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:8000', //Cambiar por la URL de producción de Django cuando despliegue
+          target: 'http://127.0.0.1:8000', 
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''), 
         },
